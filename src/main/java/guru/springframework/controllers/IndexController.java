4ef1d50303381c9f8758a560bf07bf5d45d631ad
John Bozarov <codinginusa@gmail.com>
2020-09-26 15:03:49 -0400

    repository added

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,39 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
 
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	
+	
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
+
+
 	@RequestMapping({"", "/", "/index"})
 	public String getIndexPage() {
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("American"); 
+		Optional<UnitOfMeasure> unitOfMeasurmentOptional = unitOfMeasureRepository.findByDescription("Teaspoon"); 
+		
+		System.out.println("Cat id is: " + categoryOptional.get().getId());
+		System.out.println("Unit of measure is " + unitOfMeasurmentOptional.get().getId());
+		
 		return "index"; 
 	}
 }
